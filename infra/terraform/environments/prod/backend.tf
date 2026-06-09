f243dcca2fca036aaf6d111eea31f39ff86494c3
BryanS1996 <bryanfabricio96@hotmail.com>
2026-06-09 17:20:29 -0500

    ci(infra): configure S3 backend and update bootstrap workflow for multi-env support

--- infra/terraform/environments/prod/backend.tf
+++ infra/terraform/environments/prod/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "uce-lab-tfstate-667958273162-prod"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "uce-lab-tflock-prod"                
+    encrypt        = true
+  }
 }
--- infra/terraform/environments/qa/backend.tf
+++ infra/terraform/environments/qa/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "uce-lab-tfstate-906307926859-qa"
+    key            = "qa/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "uce-lab-tflock-qa"
+    encrypt        = true
+  }
 }
