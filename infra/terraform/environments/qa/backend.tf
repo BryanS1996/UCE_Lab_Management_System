e819acc0f9b444cad542bf170dd4d4a1ec583dd9
BryanS1996 <bryanfabricio96@hotmail.com>
2026-06-12 10:50:17 -0500

    feat(infra): successfully deployed HA architecture, ASG, and dynamic workflows

--- infra/terraform/environments/qa/backend.tf
+++ infra/terraform/environments/qa/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "uce-lab-tfstate-906307926859-qa"
-    key            = "qa/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "uce-lab-tflock-qa"
-    encrypt        = true
+    bucket       = "uce-lab-tfstate-906307926859-qa"
+    key          = "qa/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
