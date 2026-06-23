a5d92beb29b91bdc543256a46da129d4fd081e6d
BryanS1996 <bryanfabricio96@hotmail.com>
2026-06-23 14:32:31 -0500

    infra(terraform): add catalog-service ECR repository and ports to QA and Prod

--- infra/terraform/environments/qa/main.tf
+++ infra/terraform/environments/qa/main.tf
@@ -4,11 +4,12 @@ locals {
     "uce-reservation-service",
     "uce-laboratory-service",
     "uce-notification-service",
+    "uce-catalog-service",
     "uce-api-gateway",
     "uce-frontend",
   ]
   # ¡Aquí está el cambio! Agregamos el puerto 80 para permitir el tráfico del Balanceador y el Health Check
-  service_ports    = [80, 3010, 3011, 3012, 3013]
+  service_ports    = [80, 3010, 3011, 3012, 3013, 3014]
   
   # ALB forwards public traffic to port 80 (nginx frontend).
   # Nginx serves the React SPA and proxies /api/* calls to the backend services.
